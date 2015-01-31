3cb522af5ddd2382304f91c18b9f319fdd193bca
aiya000 <aiya000.develop@gmail.com>
2015-01-31 15:14:21 +0900

    Learned Haskell

--- Haskell/Learn/0_Books/Haskell_no_usui_Book/P_55.hs
+++ Haskell/Learn/0_Books/Haskell_no_usui_Book/P_55.hs
@@ -5,12 +5,17 @@ main = do
   print $ replicate' 3 True
   print $ pyths 10
   print $ perfects 500
-  --print funnyGenerate
+  print funnyGenerate
+
 
 sumOfPowers = sum [x^2 | x <- [1..100]]
+
 replicate' n x = [x | _ <- [1..n]]
+
 pyths n = [(x,y,z) | x <- [2..n], y <- [2..n], z <- [2..n], x^2 + y^2 == z^2]
+
 perfects n = [x | x <- [1..n], sum (factors x) == x]
   where factors m = [y | y <- [1..m-1], m `mod` y == 0]
---funnyGenerate = [halfZip x [1,2,3] | x <- [1,2,3]]
---  where halfZip a xs = zip (repeat a) xs
+
+funnyGenerate = concat [halfZip x [1,2,3] | x <- [1,2,3]]
+  where halfZip a xs = zip (repeat a) xs
