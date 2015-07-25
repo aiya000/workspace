75482e692872a7f44f4451917b96e0fcd2eb82d1
aiya000 <aiya000.develop@gmail.com>
2015-07-25 17:16:57 +0900

    Learn Haskell: cmdargs is joyfull!!

--- Haskell/Library/CmdArgs/hello_cmdargs.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE DeriveDataTypeable #-}
-import System.Console.CmdArgs
-
-data Hello = Hello { hello :: String } deriving (Show, Data, Typeable)
-
-hi = Hello { hello = def &= help "World argument" &= opt "world" }
-        &= summary "Hello, CmdArgs !!"
-
-main = cmdArgs hi >>= print
