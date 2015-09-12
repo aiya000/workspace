5429fc014dfd9a1a363bc825c0360fce4c9354d3
aiya000 <aiya000.develop@gmail.com>
2015-09-13 04:25:56 +0900

    Learned JavaScript: Promise

---
+++ JavaScript/StdClass/hello_promise.js
@@ -0,0 +1,28 @@
+var HelloPromiss = {
+	asyncGetFoo: function () {
+		return new Promise(function (resolve, reject) {
+			var RIGHT_VAL = 4999850001;
+			var sum       = 0;
+			for (var i = 0; i < 99999; ++i) {
+				sum += i;
+			}
+	
+			if (sum != RIGHT_VAL) {
+				reject(sum);
+				return;
+			}
+			resolve(sum);
+		});
+	}
+
+	, start: function () {
+		HelloPromiss.asyncGetFoo().then(function (rightData) {
+			console.log("Succed calculate: " + rightData);
+		}, function (leftData) {
+			console.log("Calculare failure");
+			console.log("result: " + leftData);
+		});
+	}
+}
+
+HelloPromiss.start();
