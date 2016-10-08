99e1dfe9750d41adec8c961a45a8f69052341350
aiya000 <aiya000.develop@gmail.com>
2016-10-08 18:12:31 +0900

    Learn Scala typealias

--- Scala/Commons/Types/TypeAlias.scala
+++ Scala/Commons/Types/TypeAlias.scala
@@ -1,8 +1,11 @@
 object TypeAlias {
+    type O = Option[Int]
+    type E[A] = Either[String, A]
     def main(args: Array[String]): Unit = {
-        //val o = Option[Int]
-        val o = Option
-        val x = o(10)
+        val x: O = Some(10)
         println(x)
+        val y: E[Int] = Left("ahoge")
+        val z: E[Int] = Right(10)
+        println(y, z)
     }
 }
