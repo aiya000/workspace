137436ea8a05bf340b3a8086cbae69ee27474200
aiya000 <aiya000.develop@gmail.com>
2016-12-24 16:02:43 +0900

    Learn pyplot with numpy

---
+++ Python/README.md
@@ -0,0 +1,1 @@
+Use `mkvirtualenv workspace` with virtualenv and virtualenv wrapper
---
+++ Python/matplotlib/pyplot/main.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.arange(0.0, 1.0, 0.1)
+y = x ** 2
+
+plt.plot(x, y)
+plt.show()
