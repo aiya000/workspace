7141668057872b361c37a232004b27c7f1f6b3d7
aiya000 <aiya000.develop@gmail.com>
2016-12-28 12:40:31 +0900

    にこちゃん〜

--- Python/tensorflow/expressions.py
+++ Python/tensorflow/expressions.py
@@ -1,10 +1,21 @@
 # See https://www.tensorflow.org/api_docs/python/math_ops/
 import tensorflow as tf
 
+# Simply
+print("- first -")
+
+x     = tf.constant(1, name="x")
+y     = tf.Variable(x + 1, name="y")
+model = tf.global_variables_initializer()
+with tf.Session() as sess:
+    sess.run(model)
+    print(sess.run(y))
+
+
 # N-ary trees
 #    add -- reference of `counter`
 #        \_ 1
-print("- first -")
+print("- second -")
 
 # Define the variable of `counter`
 var     = tf.Variable(0, name="counter")
@@ -22,7 +33,7 @@ with tf.Session() as sess:
 
 
 # The expression of `(add (1 (sub (3 2))))`
-print("- second -")
+print("- third -")
 
 var     = tf.Variable(0, name="bar")
 new_val = tf.add(tf.constant(1), tf.subtract(tf.constant(3), tf.constant(2)))
