fb9dd9daec0e2d6b4de6af33244d85dfa6138c3f
aiya000 <aiya000.develop@gmail.com>
2019-07-16 17:38:57 +0900

    :+1: It is just intersection types

--- TypeScript/types/extend.ts
+++ TypeScript/types/extend.ts
@@ -1,20 +1,10 @@
-type Extend<Super extends object, Child extends object> = {
-  [Key in (keyof Super | keyof Child)]
-  : Key extends keyof Super
-  ? Super[Key]
-  : Key extends keyof Child
-  ? Child[Key]
-  : never  // ここは通らんぜよ
-}
-
-const x: Extend<{ x: number }, { y: string }> = {
+const a: { x: number } & { y: string } = {
   x: 10,
   y: 'hi',
 }
 
-const y: Extend<{ x: string }, { x: number }> = {
-  x: 'hi',
-}
+// 2322: Type 'string' is not assignable to type 'string & number'.  Type 'string' is not assignable to type 'number'.
+// const b: { x: string } & { x: number } = { x: 'hi' }
 
 // 2322: Type 'number' is not assignable to type 'string'.
-// const z: Extend<{x: string}, {x: number}> = { x: 10 }
+// const c: Extend<{x: string}, {x: number}> = { x: 10 }
